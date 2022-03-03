81aca705b2d6d9033e64077a288409d4c41fa15c
Moodboard123 <100765501+Moodboard123@users.noreply.github.com>
2022-03-03 20:01:16 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "guest001" && b=="a1a1")
+  	{
+  	    window.location.href="Birthday.html";
+  	}
+	else if(a== "guest002" && b=="b2b2")
+  	{
+  	    window.location.href="Birthday2.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
